c399abf6716e00ad3a54c589991c50d041ee4fa6
boryspimpek <borysgame@gmail.com>
2025-10-18 17:21:55 +0200

    w

--- main.py
+++ main.py
@@ -8,33 +8,39 @@ nogi FR i BL są w tej samej fazie zaczynają ruch po lini. """
 
 import math
 import time
+import keyboard
 import numpy as np
+from itertools import cycle
 from st3215 import ST3215
 
 servo = ST3215('COM3')
 
-# Konfiguracja
+# Servo settings
 ## LF [1,2], FR [3,4], BL [5,6], BR [7,8]
-
 sts_id = [1, 2, 3, 4, 5, 6, 7, 8,]
 acc = 250
 speed = 2400
+
+# Leg settings
 r1 = 40.0
 r2 = 50.0  
 d = 30.0
+
+# Trot gait settings
 t_points = 10
-swing_width = 20
-swing_height = 10
-base_y = -80
+swing_width = 25
+swing_height = 20
+base_y = -60
 x_offset = 0
 
-# Inicjalizacja serw
+#####################################
+
 for id in sts_id:
     servo.SetMode(id, 0)
     servo.SetAcceleration(id, acc)
     servo.SetSpeed(id, speed)
 
-def solve_ik(x_target, y_target, d=d, r1=r1, r2=r2):
+def solve_ik(x_target, y_target):
     L1 = math.sqrt((x_target - d/2)**2 + y_target**2)
     phi2 = math.atan2(y_target, x_target - d/2)
     alpha2 = math.acos((r1**2 + L1**2 - r2**2) / (2 * r1 * L1))
@@ -47,65 +53,62 @@ def solve_ik(x_target, y_target, d=d, r1=r1, r2=r2):
 
     return theta1, theta2
 
-def trot_gait(leg_pair):
-    # Trajectory for FL 
-    if leg_pair == "FL":         
-        points = []
-        half_width = swing_width / 2
-        for i in range (20):
-            for angle in np.linspace(math.pi, 0, t_points, endpoint=False):
-                x = -half_width * math.cos(angle) + x_offset
-                y = base_y + swing_height * math.sin(angle)
-                points.append((x, y))
+def trot_gait_cycle(leg_pair):
+    points = []
+    half_width = swing_width / 2
 
-            for t in np.linspace(0, 1, t_points, endpoint=False):  
-                x = -half_width + swing_width * t + x_offset
-                y = base_y
-                points.append((x, y))
+    if leg_pair == "FL":
+        # faza łuku
+        for angle in np.linspace(math.pi, 0, t_points, endpoint=False):
+            x = -half_width * math.cos(angle) + x_offset
+            y = base_y + swing_height * math.sin(angle)
+            points.append((x, y))
+        # faza liniowa
+        for t in np.linspace(0, 1, t_points, endpoint=False):
+            x = -half_width + swing_width * t + x_offset
+            y = base_y
+            points.append((x, y))
 
-        return points
+    if leg_pair == "BL":
+        # faza liniowa
+        for t in np.linspace(0, 1, t_points, endpoint=False):
+            x = -half_width + swing_width * t + x_offset
+            y = base_y
+            points.append((x, y))
+        # faza łuku
+        for angle in np.linspace(0, math.pi, t_points, endpoint=False):
+            x = half_width * math.cos(angle) + x_offset
+            y = base_y + swing_height * math.sin(angle)
+            points.append((x, y))
 
-    # Trajectory for  BL leg
-    if leg_pair == "BL":        
-        points = []
-        half_width = swing_width / 2
-        for i in range (20):
-            for t in np.linspace(0, 1, t_points, endpoint=False):
-                x = -half_width + swing_width * t + x_offset
-                y = base_y
-                points.append((x, y))
+    return cycle(points)  # ZWRACAMY CYKLICZNĄ TRAJEKTORIĘ!
 
-            for angle in np.linspace(0, math.pi, t_points, endpoint=False):
-                x = half_width * math.cos(angle) + x_offset
-                y = base_y + swing_height * np.sin(angle)
-                points.append((x, y))
-
-        return points
-
-first_trajectory = trot_gait("FL")
-second_trajectory = trot_gait("BL")
-
-for i in range(len(first_trajectory)):
-    # LF (1,2) - First Trajectory
-    x, y = first_trajectory[i]
-    th1, th2 = solve_ik(x, y)
+def stand_still():
+    th1, th2 = solve_ik(0, base_y)
     servo.WritePosition(1, servo.rad_to_servo(th1))
     servo.WritePosition(2, servo.rad_to_servo(th2))
-
-    # # RB (7,8) - First Trajectory
-    # th1, th2 = solve_ik(x, y)
-    # servo.WritePosition(8, servo.rad_to_servo(th1))
-    # servo.WritePosition(7, servo.rad_to_servo(th2))
-
-    # RF (3,4) - Second Trajectory
-    x2, y2 = second_trajectory[i]
-    th1, th2 = solve_ik(x2, y2)
     servo.WritePosition(4, servo.rad_to_servo(th1))
     servo.WritePosition(3, servo.rad_to_servo(th2))
 
-    # # LB (5,6) - Second Trajectory
-    # th1, th2 = solve_ik(x2, y2)
-    # servo.WritePosition(5, servo.rad_to_servo(th1))
-    # servo.WritePosition(6, servo.rad_to_servo(th2))
+def start_trot():
+    traj_FL = trot_gait_cycle("FL")
+    traj_BL = trot_gait_cycle("BL")
 
-    time.sleep(0.06)
+    while True:
+        if keyboard.is_pressed("w"):
+            x, y = next(traj_FL)
+            th1, th2 = solve_ik(x, y)
+            servo.WritePosition(1, servo.rad_to_servo(th1))
+            servo.WritePosition(2, servo.rad_to_servo(th2))
+
+            x2, y2 = next(traj_BL)
+            th1, th2 = solve_ik(x2, y2)
+            servo.WritePosition(4, servo.rad_to_servo(th1))
+            servo.WritePosition(3, servo.rad_to_servo(th2))
+        
+        else:
+            stand_still()
+
+        time.sleep(0.06)
+
+start_trot()
--- trash/main.py
+++ trash/main.py
@@ -1,181 +1,114 @@
+""" To jest kod realizujący trot gait dla quadruped robot dog. 
+Robot ma 4 nogi po dwa serwa na każdą nogę. 
+Konstrukcja nogi wygląda jak 2-DOF planar parallel manipulator. 
+Serwa dla kazdej nogi zamontowane są na tym samym poziomie obok siebie, 
+odleglośc miedzy osiami serw wynosi d mm. Każda noga ma dwa ogniwa o długościach r1 i r2 mm. 
+Nogi FL i BR są w tej samej fazie, zaczynają ruch po łuku, 
+nogi FR i BL są w tej samej fazie zaczynają ruch po lini. """
+
 import math
 import time
-from matplotlib import pyplot as plt
 import numpy as np
 from st3215 import ST3215
 
 servo = ST3215('COM3')
 
+# Konfiguracja
+## LF [1,2], FR [3,4], BL [5,6], BR [7,8]
 
-"""
-To jest kod realizujący trot gait dla quadruped robot dog. 
-Robot ma 4 nogi po dwa serwa na każdą nogę. 
-Konstrukcja nogi wygląda jak 2-DOF planar parallel manipulator. 
-Serwa dla kazdej nogi zamontowane są na tym samym poziomie obok siebie,
-odleglośc miedzy osiami serw wynosi d mm.
-Każda noga ma dwa ogniwa o długościach r1 i r2 mm.
-Nogi FL i BR są w tej samej fazie, zaczynają ruch po łuku,
-nogi FR i BL są w tej samej fazie zaczynają ruch po lini.
-"""
-
-# Konfiguracja robota
-r1 = 40.0  # długość pierwszego ogniwa
-r2 = 50.0  # długość drugiego ogniwa  
-d = 30.0   # odległość między serwami na podstawie
-
-# ID serw
-FL_ids = [1, 2]  # Front Left
-FR_ids = [3, 4]  # Front Right  
-BL_ids = [5, 6]  # Back Left
-BR_ids = [7, 8]  # Back Right
-
+sts_id = [1, 2, 3, 4, 5, 6, 7, 8,]
+acc = 250
+speed = 2400
+r1 = 40.0
+r2 = 50.0  
+d = 30.0
 t_points = 10
+swing_width = 20
+swing_height = 10
+base_y = -80
+x_offset = 0
 
-def rad_to_servo(rad):
-    """Zamienia kąt w radianach na wartość serwa (0-4095)"""
-    center = 2048
-    scale = 2048 / 3.1415926535
-    return 4095 - int(round(rad * scale + center))
+# Inicjalizacja serw
+for id in sts_id:
+    servo.SetMode(id, 0)
+    servo.SetAcceleration(id, acc)
+    servo.SetSpeed(id, speed)
 
 def solve_ik(x_target, y_target, d=d, r1=r1, r2=r2):
-    L1 = math.sqrt((x_target - d/2)**2 + (y_target)**2)
+    L1 = math.sqrt((x_target - d/2)**2 + y_target**2)
     phi2 = math.atan2(y_target, x_target - d/2)
     alpha2 = math.acos((r1**2 + L1**2 - r2**2) / (2 * r1 * L1))
-    theta2 = phi2 + alpha2  # elbow_out=True
+    theta2 = phi2 + alpha2
 
-    L2 = math.sqrt((x_target - (-d/2))**2 + (y_target)**2)
-    phi1 = math.atan2(y_target, x_target - (-d/2))
+    L2 = math.sqrt((x_target + d/2)**2 + y_target**2)
+    phi1 = math.atan2(y_target, x_target + d/2)
     alpha1 = math.acos((r1**2 + L2**2 - r2**2) / (2 * r1 * L2))
-    theta1 = phi1 - alpha1 # elbow_out=False
-
-    # print(f"L2 = {L2:.2f}")    
-    # print(f"phi2 deg: {math.degrees(phi2):.2f}")
-    # print(f"alpha2 deg: {math.degrees(alpha2):.2f}")
-    # print(f"THETA2 = {math.degrees(theta2):.2f}")
-    # print()
-    # print(f"L1 = {L1:.2f}")    
-    # print(f"phi1 deg: {math.degrees(phi1):.2f}")
-    # print(f"alpha1 deg: {math.degrees(alpha1):.2f}")
-    # print(f"THETA1 = {math.degrees(theta1):.2f}")
-    # print()
+    theta1 = phi1 - alpha1
 
     return theta1, theta2
 
-def generateSwing():
-    points = []
+def trot_gait(leg_pair):
+    # Trajectory for FL 
+    if leg_pair == "FL":         
+        points = []
+        half_width = swing_width / 2
+        for angle in np.linspace(math.pi, 0, t_points, endpoint=False):
+            x = -half_width * math.cos(angle) + x_offset
+            y = base_y + swing_height * math.sin(angle)
+            points.append((x, y))
+
+        for t in np.linspace(0, 1, t_points, endpoint=False):  
+            x = -half_width + swing_width * t + x_offset
+            y = base_y
+            points.append((x, y))
+
+        return points
+
+    # Trajectory for  BL leg
+    if leg_pair == "BL":        
+        points = []
+        half_width = swing_width / 2
+        for t in np.linspace(0, 1, t_points, endpoint=False):
+            x = -half_width + swing_width * t + x_offset
+            y = base_y
+            points.append((x, y))
+
+        for angle in np.linspace(0, math.pi, t_points, endpoint=False):
+            x = half_width * math.cos(angle) + x_offset
+            y = base_y + swing_height * np.sin(angle)
+            points.append((x, y))
+
+        return points
     
-    # FAZA 1: Przenoszenie (łuk w górę)
-    for angle in np.linspace(math.pi, 0, t_points):
-        x = -20 * math.cos(angle)  
-        y = -60 + 20 * math.sin(angle)
-        points.append((x, y))
-        # print(f"F1 Punkt: x={x:.2f}, y={y:.2f}")
+def start_trot():
+    first_trajectory = trot_gait("FL")
+    second_trajectory = trot_gait("BL")
 
-    # FAZA 2: Podpora (powrót po linii)
-    for t in np.linspace(0, 1, t_points):
-        x = -20 + 40 * t   
-        y = -60
-        points.append((x, y))
-        # print(f"F2 Punkt: x={x:.2f}, y={y:.2f}")
-    
-    return points
+    for i in range(len(first_trajectory)):
+        # LF (1,2) - First Trajectory
+        x, y = first_trajectory[i]
+        th1, th2 = solve_ik(x, y)
+        servo.WritePosition(1, servo.rad_to_servo(th1))
+        servo.WritePosition(2, servo.rad_to_servo(th2))
 
-def generateStance():
-    points = []
-    
-    # FAZA 1: Podpora (ruch poziomo)
-    for t in np.linspace(0, 1, t_points):
-        x = -20 + 40 * t  
-        y = -60
-        points.append((x, y))
-    
-    # FAZA 2: Przenoszenie (powrót po łuku)  
-    for angle in np.linspace(0, math.pi, t_points):  
-        x = 20 * math.cos(angle)  
-        y = -60 + 20 * math.sin(angle)
-        points.append((x, y))
-    
-    return points
+        # # RB (7,8) - First Trajectory
+        # th1, th2 = solve_ik(x, y)
+        # servo.WritePosition(8, servo.rad_to_servo(th1))
+        # servo.WritePosition(7, servo.rad_to_servo(th2))
 
-def home():
-    x = 0
-    y = -60
-    move_to_point(x, y)
+        # RF (3,4) - Second Trajectory
+        x2, y2 = second_trajectory[i]
+        th1, th2 = solve_ik(x2, y2)
+        servo.WritePosition(4, servo.rad_to_servo(th1))
+        servo.WritePosition(3, servo.rad_to_servo(th2))
 
-def move_to_point(x, y):
-    theta1, theta2 = solve_ik(x, y)
-    print(f"THETA1 = {math.degrees(theta1):.2f}")
-    print(f"THETA2 = {math.degrees(theta2):.2f}")
-    servo_positions = {
-        FL_ids[0]: rad_to_servo(theta1), FL_ids[1]: rad_to_servo(theta2),  # FL
-    }
-    print("Servo positions:", servo_positions)
+        # # LB (5,6) - Second Trajectory
+        # th1, th2 = solve_ik(x2, y2)
+        # servo.WritePosition(5, servo.rad_to_servo(th1))
+        # servo.WritePosition(6, servo.rad_to_servo(th2))
 
-def main():
-    traj_FL = generateSwing()
-    
-    for i in range(2 * t_points):
-        x, y = traj_FL[i]
-        t1_fl, t2_fl = solve_ik(x, y)
-        
-        target_positions = {
-            FL_ids[0]: rad_to_servo(t1_fl), 
-            FL_ids[1]: rad_to_servo(t2_fl)
-        }
-        
+        time.sleep(0.06)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# servo_positions = {1: 3757, 2: 2903}  
-# servo.SyncMoveTo(servo_positions, max_speed=800, acc=30, wait=False)
-
-# servo_positions = {1: 3239, 2: 2385}  
-# servo.SyncMoveTo(servo_positions, max_speed=800, acc=30, wait=False)
-
-
-# for i in range (20):
-#     main()   
-
-# home()
-
-# move_to_point(0, -60)
-
-# for i in range (5):
-
-#     move_to_point(-20, -60)
-#     move_to_point(20, -60)
+for i in range (20):
+    start_trot()
